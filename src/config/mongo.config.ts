b7769a50c11ea739c587cac87c7148a328813f14
brian perdomo <brayanperdomovelasquez@gmail.com>
2023-06-30 21:52:14 -0500

    [+] mongo.confing:se define el entorno de URI

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 // TODO:Resolver conexion de MOngoDB con Mongoose
 async function dbConnect () : Promise<void> {
-    const DB_URI = 'mongodb://127.0.0.1:27017/store';
+    const DB_URI = `${process.env.DB_URI}`;
     await connect ( DB_URI );
 }
 

b6ced39cffc5598ec600461089e0734eae7636b8
brian perdomo <brayanperdomovelasquez@gmail.com>
2023-06-30 21:37:40 -0500

    [+]mongo.confing: agregamos al URI de Mongo el nombre de la base de datos

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 // TODO:Resolver conexion de MOngoDB con Mongoose
 async function dbConnect () : Promise<void> {
-    const DB_URI = 'mongodb://127.0.0.1:27017';
+    const DB_URI = 'mongodb://127.0.0.1:27017/store';
     await connect ( DB_URI );
 }
 
